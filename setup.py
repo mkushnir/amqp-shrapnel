10971bb1d6b7fe764d0b64f55623ca53616b2a2c
Markiyan Kushnir <mkushnir@llnw.com>
2013-02-01 14:43:56 -0700

    git paths fixed

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup (
     description       = "AMQP for Shrapnel",
     license           = "Simplified BSD",
     keywords          = "amqp shrapnel",
-    url               = 'http://github.com/samrushing/amqp-shrapnel/',
-    download_url      = "http://github.com/samrushing/amqp-shrapnel/tarball/v0.5#egg=amqp-shrapnel",
+    url               = 'http://github.com/mkushnir/amqp-shrapnel/',
+    download_url      = "http://github.com/mkushnir/amqp-shrapnel/tarball/v0.5#egg=amqp-shrapnel",
     install_requires  = ['coro'],
     dependency_links  = ['http://github.com/ironport/shrapnel/tarball/master#egg=coro-0.1'],
     zip_safe          = False
